68c24fac7ad249c752311943c3ee1cabe8e31740
Gabi-Soare <gabyz_me@yahoo.com>
2014-01-06 16:19:03 +0200

    add

--- www/js/activities.js
+++ www/js/activities.js
@@ -10,9 +10,9 @@ function querySuccess(tx, results) {
     var len = results.rows.length;
 	
     for (var i=0; i<len; i++){
-		var row = table.insertRow(len-1-i);
+		var row = table.insertRow(i);
 		var cell = row.insertCell(0);
-		cell.innerHTML  = results.rows.item(i).desc;
+		cell.innerHTML  = results.rows.item(len-1-i).desc;
     }
 }
 

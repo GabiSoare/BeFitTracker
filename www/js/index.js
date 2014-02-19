00373e593acc8d62a0dc9eed63cd69647b934013
Gabi-Soare <gabyz_me@yahoo.com>
2014-02-19 12:13:19 +0200

    add-download-progressbar

--- www/js/index.js
+++ www/js/index.js
@@ -10,6 +10,8 @@
     var e_mail = window.localStorage.getItem("Email");
 	var username = window.localStorage.getItem("Username");
 
+	//If not Zero then Database has been downloadded
+	var qtyRows=0;
 		
 	var atpos=e_mail.indexOf("@");
 	var dotpos=e_mail.lastIndexOf(".");
@@ -31,26 +33,32 @@
 	
        
 function downloadDatabase(){
-	alert("acu`");
+	window.location.href = "loadfood.html";
 }        
     
 function noDownloadDatabase(){
-	alert("Las` ca revii tu");
 	window.localStorage.setItem("GOTOMAIN_FLAG","1234");
 }
         
 function signup(){
    	var username = document.getElementById("username").value;
-  	var email = document.getElementById("email").value;
+  	var pass = document.getElementById("pass").value;
+	var pass1 = document.getElementById("pass1").value;
+	var email = document.getElementById("email").value;
    	var mass = document.getElementById("mass").value;
  	var height = document.getElementById("height").value;
 	var gender = document.getElementById("gender").options[document.getElementById("gender").selectedIndex].text;
 	    		
-	if(username=="" || mass=="" || height==""){
+	if(username=="" || mass=="" || height=="" || pass==""){
   		alert("Please fill in all fields.");
     	return false;
     }
 
+	if(pass!=pass1){
+		alert("The password doesn't match");
+		return false;
+	}
+	
 	var x = email;
 	var atpos=x.indexOf("@");
 	var dotpos=x.lastIndexOf(".");
@@ -77,11 +85,11 @@ function signup(){
     	return false;    			
 	}
 	
-    sendData(username,email,mass,height,gender);
+    sendData(username,pass,email,mass,height,gender);
 
 }
         
-function sendData(username,email,mass,height,gender)
+function sendData(username,pass,email,mass,height,gender)
 {
 	if (window.XMLHttpRequest)
 	 {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -96,24 +104,68 @@ function sendData(username,email,mass,height,gender)
 	signup.style.display = "none";	
 	var p = document.getElementById("p"); 
   	p.style.display="block";	 
-	p.innerHTML = "Welcome here "+ username + "!<br><br> Do you like to download our Food Database? <br><a onclick='downloadDatabase()'>Yes</a> <a href='main.html' onclick='noDownloadDatabase()' rel='external' style='text-decoration:none; color: #506070'>No</a>";
-	
+
+  	/*
+		If Rows quantity is equals Zero then ask if download Foods table
+		Else goes to Main Screen.
+		*/
+		if(qtyRows==0){
+
+			p.innerHTML = "Welcome here "+ username + "!<br><br> Do you like to download our Food Database? <br><a onclick='downloadDatabase()'>Yes</a> <a href='main.html' onclick='noDownloadDatabase()' rel='external' style='text-decoration:none; color: #506070'>No</a>";
+		}
+		else{
+			window.location.href = "main.html";
+		}
+		
     window.localStorage.setItem("Username", username);
-    window.localStorage.setItem("Email", email);
+    window.localStorage.setItem("Password", pass);
+	window.localStorage.setItem("Email", email);
     window.localStorage.setItem("Mass", mass);
     window.localStorage.setItem("Height", height);
 	window.localStorage.setItem("Gender", gender);    
 
-	xmlhttp.open("GET","http://embeddedsoft.ro/befit/getuser.php?username="+encodeURIComponent(username)+"&email="+encodeURIComponent(email)+"&mass="+encodeURIComponent(mass)+"&height="+encodeURIComponent(height)+"&gender="+encodeURIComponent(gender),true);
+	xmlhttp.open("GET","http://embeddedsoft.ro/befit/getuser.php?username="+encodeURIComponent(username)+"&pass="+encodeURIComponent(pass)+"&email="+encodeURIComponent(email)+"&mass="+encodeURIComponent(mass)+"&height="+encodeURIComponent(height)+"&gender="+encodeURIComponent(gender),true);
 	xmlhttp.send();
 }
-
 
 
     // PhoneGap is ready
     //
     function onDeviceReady() {
-    	p.innerHTML = "Merje";
-    }
 
-		
+    	//This not will create anything only is for Query purposes. And extract if Data must
+    	//to be downladed. Really loadfood.js will create Tables.
+    	var db = openDatabase("test", "1.0", "Test DB", 50000000);
+    	db.transaction(populateDB, errorCB, successCB);
+    	function populateDB(tx) {
+
+    	}
+
+    	function errorCB(err) {
+    		console.log("Error processing SQL: " + err.code);
+    	}	
+
+    	function successCB() {
+    		console.log("success!");
+    		db.transaction(queryI, errorI);
+    	}
+    	function queryI(tx) {
+					// tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+					tx.executeSql('SELECT * FROM FOODS',[],querySuccess,errorCB);
+
+	        		//console.log("Completed : " + offset);
+	        	}
+	        	function errorI(err) {
+	        		alert(err.code);
+	        		console.log("Error processing SQL: " + err.code);
+	        	}	
+	        	function querySuccess(tx,results) {
+	        		var len = results.rows.length;
+	        		qtyRows = len;
+	        		console.log("FOODS table: " + len + " rows found.");
+	        		for (var i=0; i<len; i++){
+	        			console.log("Row = " + i + " ID = " + results.rows.item(i) + " Data =  " + results.rows.item(i).data);
+	        		}	    			
+	        	}
+	        }
+

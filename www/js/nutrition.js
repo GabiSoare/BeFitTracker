0e82a96c6f96e4e7f611217771586707d0fa43d3
Gabi-Soare <gabyz_me@yahoo.com>
2014-02-27 01:07:54 +0200

    clearinput

--- www/js/nutrition.js
+++ www/js/nutrition.js
@@ -9,6 +9,7 @@ var Protein = document.getElementById('Protein').value;
 var Lipid = document.getElementById('Lipid').value;
 var Carbohydrt = document.getElementById('Carbohydrt').value;
 var Fiber = document.getElementById('Fiber').value;
+var Sugar = document.getElementById('Sugar').value;
 var Calcium = document.getElementById('Calcium').value;
 var Iron = document.getElementById('Iron').value;
 var Magnesium = document.getElementById('Magnesium').value;
@@ -18,14 +19,20 @@ var FA_Mono = document.getElementById('FA_Mono').value;
 var FA_Poly = document.getElementById('FA_Poly').value;
 var Cholestrl = document.getElementById('Cholestrl').value;
 
+var items= ["Desc","Water","Energy","Protein","Lipid","Carbohydrt","Fiber","Sugar","Calcium","Iron","Magnesium","Vit_C","FA_Sat","FA_Mono","FA_Poly","Cholestrl"];
 
 var db_info = "`Desc`,`Water_(g)`,`Energ_Kcal`,`Protein_(g)`,`Lipid_Tot_(g)`,`Carbohydrt_(g)`,`Fiber_TD_(g)`,`Sugar_Tot_(g)`,`Calcium_(mg)`,`Iron_(mg)`,`Magnesium_(mg)`,`Vit_C_(mg)`,`FA_Sat_(g)`,`FA_Mono_(g)`,`FA_Poly_(g)`,`Cholestrl_(mg)`";
+
+function clearInput(){
+	for(var i=0;i<items.length;i++){
+		document.getElementById(items[i]).value="";
+	}
+}
 
 function populateDB(tx) {
     tx.executeSql("CREATE TABLE IF NOT EXISTS FOOD ("+db_info+")");
 	tx.executeSql('INSERT INTO FOOD ('+db_info+') VALUES ("'+Desc+'","'+Water+'","'+Energy+'","'+Protein+'","'+Lipid+'","'+Carbohydrt+'","'+Fiber+'","'+Sugar+'","'+Calcium+'","'+Iron+'","'+Magnesium+'","'+Vit_C+'","'+FA_Sat+'","'+FA_Mono+'","'+FA_Poly+'","'+Cholestrl+'")');
-
-//	food_name.value= energy.value = protein.value = lipid.value = carbo.value = "";
+	clearInput();
 }
 
 // Transaction error callback
@@ -56,7 +63,7 @@ function addOption(){
 
 
 function dontAddFood(){
-//	food_name.value= energy.value = protein.value = lipid.value = carbo.value = "";
+	clearInput();
 	table.style.display = "block";
 	add_new.style.display = 'none';	
 }

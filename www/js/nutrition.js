571696caafdf3d875459a7c6013a14800fad98f2
Gabi-Soare <gabyz_me@yahoo.com>
2014-02-27 00:27:38 +0200

    ad

--- www/js/nutrition.js
+++ www/js/nutrition.js
@@ -3,12 +3,20 @@ var table = document.getElementById('nutrition-table');
 var add_new = document.getElementById('add-new');
 
 var Desc = document.getElementById('Desc').value;
+var Water = document.getElementById('Water').value;
+var Energ = document.getElementById('Energ').value;
+var Protein = document.getElementById('Protein').value;
+var Lipid = document.getElementById('Lipid').value;
+var Carbohydrt = document.getElementById('Carbohydrt').value;
+var Fiber = document.getElementById('Fiber').value;
+
+
 
 var db_info = "`Desc`,`Water_(g)`,`Energ_Kcal`,`Protein_(g)`,`Lipid_Tot_(g)`,`Carbohydrt_(g)`,`Fiber_TD_(g)`,`Sugar_Tot_(g)`,`Calcium_(mg)`,`Iron_(mg)`,`Magnesium_(mg)`,`Vit_C_(mg)`,`FA_Sat_(g)`,`FA_Mono_(g)`,`FA_Poly_(g)`,`Cholestrl_(mg)`";
 
 function populateDB(tx) {
     tx.executeSql("CREATE TABLE IF NOT EXISTS FOOD ("+db_info+")");
-	tx.executeSql('INSERT INTO FOOD ('+db_info+') VALUES ("'+Desc+'","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1")');
+	tx.executeSql('INSERT INTO FOOD ('+db_info+') VALUES ("'+Desc+'","'+Water+'","'+Energ+'","'+Protein+'","'+Lipid+'","'+Carbohydrt+'","'+Fiber+'","1","1","1","1","1","1","1","1","1")');
 
 //	food_name.value= energy.value = protein.value = lipid.value = carbo.value = "";
 }
